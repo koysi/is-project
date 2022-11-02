f1b34f42011bef297b137e023be8637196e6d413
koysi <coy3gause@gmail.com>
2022-11-02 12:02:31 -0400

    added buttons

--- form.php
+++ form.php
@@ -1,7 +1,36 @@
 <?php
 
+if(isset($_POST['but_home'])){
+    header('Location: home.php');
+}
+
+if(isset($_POST['but_logout'])){
+    session_destroy();
+    header('Location: index.php');
+}
+
 ?>
 <!DOCTYPE html>
 <html>
-    
+    <head>
+        <title>Reporting Form</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div>
+            <form method='post' action="">
+                <input type="submit" value="Home" name="but_home">
+                <input type="submit" value="Logout" name="but_logout">
+            </form>
+        </div>
+        <div>
+            <input type="text" class="textbox" id="barname" name="barname" />
+        </div>
+        <div>
+            <input type="text" class="textbox" id="man_name" name="man_name" />
+        </div>
+        <div>
+            <input type="text" class="textbox" id="txt_date" name="txt_date" />
+        </div>
+    </body>
 </html>
