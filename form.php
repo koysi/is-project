23cb4cc349978c9fe34d9e71450897c5f1bf9a4c
koysi <coy3gause@gmail.com>
2022-11-02 12:30:46 -0400

    added labels

--- form.php
+++ form.php
@@ -22,14 +22,20 @@ if(isset($_POST['but_logout'])){
                 <input type="submit" value="Home" name="but_home">
                 <input type="submit" value="Logout" name="but_logout">
             </form>
+            <br>
         </div>
         <div>
+            <label for="barname">Bar Name: </label>
             <input type="text" class="textbox" id="barname" name="barname" />
+            <br>
         </div>
         <div>
+            <label for="man_name">Manager: </label>
             <input type="text" class="textbox" id="man_name" name="man_name" />
+            <br>
         </div>
         <div>
+            <label for="txt_date">Date: </label>
             <input type="text" class="textbox" id="txt_date" name="txt_date" />
         </div>
     </body>
