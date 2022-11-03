7b7639d98fa12da8abffdd68321f8030a823a6c4
koysi <coy3gause@gmail.com>
2022-11-03 15:35:54 -0400

    file to test the behavior of tables that feature input

---
+++ tabletest.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Testing Tables</title>
+    </head>
+
+    <body>
+        <h1>Table</h1>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="header-label">Index</th>
+                    <th class="header-label">Name</th>
+                    <th class="header-label">Phone</th>
+                    <th class="header-label">Company</th>
+                    <th class="header-label">Registered</th>
+                    <th class="header-label">Is Active</th>
+                </tr>
+                <tr class="tr-inputs">
+                    <th>
+                        <div><input type="number"></div>
+                    </th>
+                    <th>
+                        <div><input type="string"></div>
+                    </th>
+                    <th>
+                        <div><input type="string"></div>
+                    </th>
+                    <th>
+                        <div><input type="string"></div>
+                    </th>
+                    <th>
+                        <div><input type="date"></div>
+                    </th>
+                    <th>
+                        <div><input type="boolean"></div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>0</td>
+                    <td>Coy Gause</td>
+                    <td>+1 (336) 830-0072</td>
+                    <td>CACI</td>
+                    <td>2022-11-3T15:29:33 -01:00</td>
+                    <td>true</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Knox Holman</td>
+                    <td>+1 (880) 497-2808</td>
+                    <td>MAINELAND</td>
+                    <td>2017-05-07T02:54:22 -01:00</td>
+                    <td>false</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Brandy Colon</td>
+                    <td>+1 (969) 513-2827</td>
+                    <td>NEXGENE</td>
+                    <td>2017-06-07T06:42:31 -00:00</td>
+                    <td>true</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
