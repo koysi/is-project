26be37ae96e8122cb42d4b18940ef6131af6b0f6
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-10 16:46:19 -0500

    New navbar

--- home.php
+++ home.php
@@ -8,25 +8,22 @@ if(!isset($_SESSION['uname'])){
 
 if(isset($POST['btnLogout'])){
 	session_destroy();
-}
-?>
+} 
+
+?> 
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="homestyle.css">
-</head>
-<body>
-	<form method="post" action="">
-		<nav class="navbar background">
-			<ul class="nav-list">
-				<li><a href="home.php" class="active">Home</a></li>
-				<li><a href="reports.php">Reporting</a></li>
-				<li><a href="transfer.php">Transfers</a></li>
-				<li><a href="register.php">Register</a></li>
-				<li><a href="index.php" id="btnLogout">Logout</a></li>
-			</ul>
-		</nav>
-	</form>
-	<h1>Homepage</h1>
-</body>
+        <title>Register</title>
+        <link rel="stylesheet" href="homestyle.css">
+    </head>
+    <body>
+    <ul>
+        <li><a class="active" href="home.php">Home</a></li>
+        <li><a href="reports.php">Reports</a></li>
+        <li><a href="transfer.php">Transfers</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li style="float:right"><a href="index.php">Logout</a></li>
+    </body>
+    </ul>
 </html>
--- register.php
+++ register.php
@@ -5,15 +5,12 @@
         <link rel="stylesheet" href="homestyle.css">
     </head>
     <body>
-        <nav class="navbar background">
-            <ul class="nav-list">
-                <li><a href="home.php">Home</a></li>
-                <li><a href="reports.php">Reporting</a></li>
-                <li><a href="transfer.php">Transfers</a></li>
-                <li><a href="register.php" class="active">Register</a></li>
-                <li><a href="index.php">Logout</a></li>
-            </ul>
-        </nav>
-        <h1>Register New User</h1>
+    <ul>
+        <li><a class="active" href="home.php">Home</a></li>
+        <li><a href="reports.php">Reports</a></li>
+        <li><a href="transfer.php">Transfers</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li style="float:right"><a href="index.php">Logout</a></li>
     </body>
+    </ul>
 </html>
