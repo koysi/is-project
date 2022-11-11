843b055008bb4e559d8aa666d2f4b26990225503
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-11 12:35:17 -0500

    Styling update to form

--- reports.php
+++ reports.php
@@ -27,15 +27,15 @@ delete from srcbar, add to destbar-->
             <label for="barid">Bar ID: </label>
             <input type="number" class="textbox" id="barid" name="barid"/>
         </div>
-        </form><br>
+        </form>
 
         <form>
-        <div>
+        <div class="div2">
             <label for="barname">Bar Name: </label>
             <input type="text" class="textbox" id="barname" name="barname" />
             <br>
         </div>
-        </form><br>
+        </form>
 
         <form>
         <div>
@@ -43,14 +43,14 @@ delete from srcbar, add to destbar-->
             <input type="text" class="textbox" id="man_name" name="man_name" />
             <br>
         </div>
-        </form><br>
+        </form>
 
         <form>
-        <div>
+        <div class="div2">
             <label for="txt_date">Date: </label>
             <input type="text" class="textbox" id="txt_date" name="txt_date" />
         </div>
-        </form><br>
+        </form>
         
         <form method="post" action="">
         <div>
@@ -59,18 +59,17 @@ delete from srcbar, add to destbar-->
             </form><br>
 <form>
             <input type="radio" id="ending" name="whenreport" value="Ending">
-            <label for="ending">Ending</label><br>
+            <label for="ending">Ending</label>
         </div>
-        </form><br>
-
+        </form>
             <form method="post" action="">
-                <div>
+                <div class="div2">
                     <label>Coors: </label>
                     <br>
                     <input type="number" class="textbox" id="begcoorcase" name="begcoorcase" placeholder="Beginning Cases" />
                     <input type="number" class="textbox" id="begcoorcan" name="begcoorcan" placeholder="Beginning Cans" />
                 </div>
-            </form><br>
+            </form>
 
             <form method="post" action="">
                 <div>
@@ -80,50 +79,43 @@ delete from srcbar, add to destbar-->
                     <input type="number" class="textbox" id="begmillercan" name="begmillercan" placeholder="Beginning Cans" />
                 </div>
 </form>
-                <br>
-                <div>
+                <div class="div2">
                     <label>Vizzy: </label>
                     <br>
                     <input type="number" class="textbox" id="begvizzycase" name="begvizzycase" placeholder="Beginning Cases" />
                     <input type="number" class="textbox" id="begvizzycan" name="begvizzycan" placeholder="Beginning Cans" />
                 </div>
-                <br>
                 <div>
                     <label>Topo Chico: </label>
                     <br>
                     <input type="number" class="textbox" id="begtopocase" name="begtopocase" placeholder="Beginning Cases" />
                     <input type="number" class="textbox" id="begtopocan" name="begtopocan" placeholder="Beginning Cans" />
                 </div>
-                <br>
-                <div>
+                <div class="div2">
                     <label>R&D Carolina: </label>
                     <br>
                     <input type="number" class="textbox" id="begrdccase" name="begrdccase" placeholder="Beginning Cases" />
                     <input type="number" class="textbox" id="begrdccan" name="begrdccan" placeholder="Beginning Cans" />
                 </div>
-                <br>
                 <div>
                     <label>R&D MTN: </label>
                     <br>
                     <input type="number" class="textbox" id="begrdmcase" name="begrdmcase" placeholder="Beginning Cases" />
                     <input type="number" class="textbox" id="begrdmcan" name="begrdmcan" placeholder="Beginning Cans" />
                 </div>
-                <br>
-                <div>
+                <div class="div2">
                     <label>Booneshine Tropic: </label>
                     <br>
                     <input type="number" class="textbox" id="begbstcase" name="begbstcase" placeholder="Beginning Cases" />
                     <input type="number" class="textbox" id="begbstcan" name="begbstcan" placeholder="Beginning Cans" />
                 </div>
-                <br>
                 <div>
                     <label>Water: </label>
                     <br>
                     <input type="number" class="textbox" id="begwatercase" name="begwatercase" placeholder="Beginning Cases" />
                     <input type="number" class="textbox" id="begwatercan" name="begwatercan" placeholder="Beginning Bottles" />
                 </div>
-                <br>
-                <div>
+                <div class="div2">
                     <input type="button" id="viewreports" name="viewreports" value="View Reports">    
                     <input type="submit" value="Submit" name="submit_but" id="submit_but" />
                 </div>
