69a025d084c67fcb659695af5d35f16221058955
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-10 21:27:13 -0500

    Navbar update

--- transfer.php
+++ transfer.php
@@ -5,15 +5,14 @@
         <link rel="stylesheet" href="homestyle.css">
     </head>
     <body>
-        <nav class="navbar background">
-            <ul class="nav-list">
-                <li><a href="home.php">Home</a></li>
-                <li><a href="reports.php">Reporting</a></li>
-                <li><a href="transfer.php" class="active">Transfers</a></li>
-                <li><a href="register.php">Register</a></li>
-                <li><a href="index.php">Logout</a></li>
-            </ul>
-        </nav>
+    <ul>
+        <li><a class="active" href="home.php">Home</a></li>
+        <li><a href="reports.php">Reports</a></li>
+        <li><a href="transfer.php">Transfers</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li style="float:right"><a href="index.php">Logout</a></li>
+    </body>
+    </ul>
         <h1>Transfer Inventory</h1><br>
         <div>
             <form method="post" action="">
