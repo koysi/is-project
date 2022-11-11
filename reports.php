00ddcbde42f98bf51ea82b6d626910ade2d0cf57
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-10 21:29:34 -0500

    Navbar update

--- reports.php
+++ reports.php
@@ -11,15 +11,14 @@ delete from srcbar, add to destbar-->
         <link rel="stylesheet" href="homestyle.css">
     </head>
     <body>
-        <nav class="navbar background">
-            <ul class="nav-list">
-                <li><a href="home.php">Home</a></li>
-                <li><a href="reports.php" class="active">Reporting</a></li>
-                <li><a href="transfer.php">Transfers</a></li>
-                <li><a href="register.php">Register</a></li>
-                <li><a href="index.php">Logout</a></li>
-            </ul>
-        </nav>
+    <ul>
+        <li><a class="active" href="home.php">Home</a></li>
+        <li><a href="reports.php">Reports</a></li>
+        <li><a href="transfer.php">Transfers</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li style="float:right"><a href="index.php">Logout</a></li>
+    </body>
+    </ul>
         <h1>Reporting</h1><br>
         <form method="post" action="">
         <div>
