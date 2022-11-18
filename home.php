9e11e605defb37cc62ee659b75dc0124980f8424
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-18 14:00:18 -0500

    update

--- home.php
+++ home.php
@@ -21,9 +21,8 @@ if(isset($POST['btnLogout'])){
     <ul>
     <li class="taftandharding" href="#"><img src="https://taftandhardingevents.com/wp-content/uploads/2022/06/taft-and-harding-final.png" height=73 width=90 alt="logo"></li>
         <b><li><a class="active" href="home.php">HOME</a></li></b>
-        <b><li><a href="reports.php">REPORTS</a></li></b>
-        <!-- <b><li><a href="transfer.php">TRANSFERS</a></li></b>
-        <b><li><a href="register.php">REGISTER</a></li></b> -->
+        <b><li><a href="reports.php">BEGINNING REPORTS</a></li></b>
+        <li><a href="endreports.php">ENDING REPORTS</a></li>
         <b><li style="float:right"><a href="index.php">LOGOUT</a></li></b>
     </body>
     </ul>
