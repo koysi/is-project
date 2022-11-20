3e6e5924e574c0513fec93df3bf9f322df7a7bfc
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-20 17:43:15 -0500

    nav link update

--- home.php
+++ home.php
@@ -3,50 +3,55 @@
 include "config.php";
 
 // Check if user is logged in
-if(!isset($_SESSION['uname'])){
-	header('Location: index.php');
+if (!isset($_SESSION['uname'])) {
+    header('Location: index.php');
 }
 
-if(isset($POST['btnLogout'])){
-	session_destroy();
-} 
+if (isset($POST['btnLogout'])) {
+    session_destroy();
+}
 
-?> 
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-  <link rel="stylesheet" href="homestyle.css">
-  <title>Hamburger Menu Overlay</title>
-</head>
-<body>
-  <div class="menu-wrap">
-    <input type="checkbox" class="toggler">
-    <div class="hamburger"><div></div></div>
-    <div class="menu">
-      <div>
-        <div>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Report Beginning Inventory</a></li>
-            <li><a href="#">Report Ending Inventory</a></li>
-            <li><a href="#">Logout</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <header class="showcase">
-    <div class="container showcase-inner">
-      <h1>Taft and Harding</h1>
-      <p>PROVIDING STAFF & MANAGEMENT FOR BALLGAMES, CONCERTS, RODEOS, FESTIVALS & MORE SINCE 2005</p>
-      <a href="https://taftandhardingevents.com/" class="btn">Main Website</a>
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="homestyle.css">
+    <title>Hamburger Menu Overlay</title>
+</head>
+
+<body>
+    <div class="menu-wrap">
+        <input type="checkbox" class="toggler">
+        <div class="hamburger">
+            <div></div>
+        </div>
+        <div class="menu">
+            <div>
+                <div>
+                    <ul>
+                        <li><a href="home.php">Home</a></li>
+                        <li><a href="reports.php">Report Beginning Inventory</a></li>
+                        <li><a href="endreports.php">Report Ending Inventory</a></li>
+                        <li><a href="index.php">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-  </header>
+
+    <header class="showcase">
+        <div class="container showcase-inner">
+            <h1>Taft and Harding</h1>
+            <p>PROVIDING STAFF & MANAGEMENT FOR BALLGAMES, CONCERTS, RODEOS, FESTIVALS & MORE SINCE 2005</p>
+            <a href="https://taftandhardingevents.com/" class="btn">Main Website</a>
+        </div>
+    </header>
 </body>
+
 </html>
