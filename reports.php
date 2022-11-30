b809cd235076239ea89188b5f18283302ec7557e
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-29 22:18:43 -0500

    Test

--- reports.php
+++ reports.php
@@ -50,84 +50,80 @@ attempted to add inventory variables to query-->
                     <h1>BEGINNING INVENTORY</h1>
                 </center>
                 <br>
-                <form>
-                    <div class="div4">
-                        <label for="barid">Bar ID: </label><br>
-                        <input type="number" class="textbox" id="barid" name="barid" style="width: 690px" />
-                    </div>
-                </form>
+                <div class="div4">
+                    <label for="barid">Bar ID: </label><br>
+                    <input type="number" class="textbox" id="barid" name="barid" style="width: 690px" />
+                </div>
 
-                <form>
-                    <div class="div2">
-                        <label for="barname">Bar Name: </label>
-                        <input type="text" class="textbox" id="barname" name="barname" style="width: 690px" />
-                        <br>
-                    </div>
-                </form>
+                <div class="div2">
+                    <label for="barname">Bar Name: </label>
+                    <input type="text" class="textbox" id="barname" name="barname" style="width: 690px" />
+                    <br>
+                </div>
 
-                <form>
-                    <div class="div4">
-                        <label for="man_name">Manager: </label>
-                        <input type="text" class="textbox" id="man_name" name="man_name" style="width: 690px" />
-                        <br>
-                    </div>
-                </form>
+                <div class="div4">
+                    <label for="man_name">Manager: </label>
+                    <input type="text" class="textbox" id="man_name" name="man_name" style="width: 690px" />
+                    <br>
+                </div>
 
-                <form>
-                    <div class="div2">
-                        <label for="txt_date">Date (yyyy-mm-dd): </label>
-                        <input type="text" class="textbox" id="txt_date" name="txt_date" style="width: 690px" />
-                    </div>
-                </form>
-                <center><img src="https://taftandhardingevents.com/wp-content/uploads/2022/06/taft-and-harding-final.png" height=150 width=180 alt="logo"></center>
-                <table id="beer">
-                    <form method="post" action="">
-                        <tr>
-                            <th>Beer</th>
-                            <th>Cases</th>
-                            <th>Cans</th>
-                        </tr>
-                        <tr>
-                            <td>Coors</td>
-                            <td><input type="number" class="textbox" id="begcoorcase" name="begcoorcase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begcoorcan" name="begcoorcan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                        <tr>
-                            <td>Miller</td>
-                            <td><input type="number" class="textbox" id="begmillercase" name="begmillercase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begmillercan" name="begmillercan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                        <tr>
-                            <td>Vizzy</td>
-                            <td><input type="number" class="textbox" id="begvizzycase" name="begvizzycase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begvizzycan" name="begvizzycan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                        <tr>
-                            <td>Topo Chico</td>
-                            <td><input type="number" class="textbox" id="begtopocase" name="begtopocase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begtopocan" name="begtopocan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                        <tr>
-                            <td>R&D Carolina</td>
-                            <td><input type="number" class="textbox" id="begrdccase" name="begrdccase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begrdccan" name="begrdccan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                        <tr>
-                            <td>R&D MTN</td>
-                            <td><input type="number" class="textbox" id="begrdmcase" name="begrdmcase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begrdmcan" name="begrdmcan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                        <tr>
-                            <td>Booneshine Tropic</td>
-                            <td><input type="number" class="textbox" id="begbstcase" name="begbstcase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begbstcan" name="begbstcan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                        <tr>
-                            <td>Water</td>
-                            <td><input type="number" class="textbox" id="begwatercase" name="begwatercase" placeholder="" style="width:200px" /></td>
-                            <td><input type="number" class="textbox" id="begwatercan" name="begwatercan" placeholder="" style="width:200px" /></td>
-                        </tr>
-                    </form>
+                <div class="div2">
+                    <label for="txt_date">Date (yyyy-mm-dd): </label>
+                    <input type="text" class="textbox" id="txt_date" name="txt_date" style="width: 690px" />
+                </div>
+            </form>
+            <form>
+                <div>
+                    <center><img src="https://taftandhardingevents.com/wp-content/uploads/2022/06/taft-and-harding-final.png" height=150 width=180 alt="logo"></center>
+                    <table id="beer">
+                        <form method="post" action="">
+                            <tr>
+                                <th>Beer</th>
+                                <th>Cases</th>
+                                <th>Cans</th>
+                            </tr>
+                            <tr>
+                                <td>Coors</td>
+                                <td><input type="number" class="textbox" id="begcoorcase" name="begcoorcase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begcoorcan" name="begcoorcan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                            <tr>
+                                <td>Miller</td>
+                                <td><input type="number" class="textbox" id="begmillercase" name="begmillercase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begmillercan" name="begmillercan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                            <tr>
+                                <td>Vizzy</td>
+                                <td><input type="number" class="textbox" id="begvizzycase" name="begvizzycase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begvizzycan" name="begvizzycan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                            <tr>
+                                <td>Topo Chico</td>
+                                <td><input type="number" class="textbox" id="begtopocase" name="begtopocase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begtopocan" name="begtopocan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                            <tr>
+                                <td>R&D Carolina</td>
+                                <td><input type="number" class="textbox" id="begrdccase" name="begrdccase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begrdccan" name="begrdccan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                            <tr>
+                                <td>R&D MTN</td>
+                                <td><input type="number" class="textbox" id="begrdmcase" name="begrdmcase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begrdmcan" name="begrdmcan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                            <tr>
+                                <td>Booneshine Tropic</td>
+                                <td><input type="number" class="textbox" id="begbstcase" name="begbstcase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begbstcan" name="begbstcan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                            <tr>
+                                <td>Water</td>
+                                <td><input type="number" class="textbox" id="begwatercase" name="begwatercase" placeholder="" style="width:200px" /></td>
+                                <td><input type="number" class="textbox" id="begwatercan" name="begwatercan" placeholder="" style="width:200px" /></td>
+                            </tr>
+                        </form>
+                </div>
                 </table>
                 <br>
                 <div>
