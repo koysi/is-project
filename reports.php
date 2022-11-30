4ad74a5d55c853a7258260560d6a95dfe8961519
graceh-13 <117241582+graceh-13@users.noreply.github.com>
2022-11-29 23:04:07 -0500

    Accidentally messed up the button

--- reports.php
+++ reports.php
@@ -71,118 +71,119 @@ attempted to add inventory variables to query-->
                     <label for="txt_date">Date (yyyy-mm-dd): </label>
                     <input type="text" class="textbox" id="txt_date" name="txt_date" style="width: 690px" />
                 </div>
-                <center><img src="https://taftandhardingevents.com/wp-content/uploads/2022/06/taft-and-harding-final.png" height=150 width=180 alt="logo"></center>
-                <table id="beer">
-                    <tr>
-                        <th>Beer</th>
-                        <th>Cases</th>
-                        <th>Cans</th>
-                    </tr>
-                    <tr>
-                        <td>Coors</td>
-                        <td><input type="number" class="textbox" id="begcoorcase" name="begcoorcase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begcoorcan" name="begcoorcan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                    <tr>
-                        <td>Miller</td>
-                        <td><input type="number" class="textbox" id="begmillercase" name="begmillercase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begmillercan" name="begmillercan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                    <tr>
-                        <td>Vizzy</td>
-                        <td><input type="number" class="textbox" id="begvizzycase" name="begvizzycase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begvizzycan" name="begvizzycan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                    <tr>
-                        <td>Topo Chico</td>
-                        <td><input type="number" class="textbox" id="begtopocase" name="begtopocase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begtopocan" name="begtopocan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                    <tr>
-                        <td>R&D Carolina</td>
-                        <td><input type="number" class="textbox" id="begrdccase" name="begrdccase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begrdccan" name="begrdccan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                    <tr>
-                        <td>R&D MTN</td>
-                        <td><input type="number" class="textbox" id="begrdmcase" name="begrdmcase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begrdmcan" name="begrdmcan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                    <tr>
-                        <td>Booneshine Tropic</td>
-                        <td><input type="number" class="textbox" id="begbstcase" name="begbstcase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begbstcan" name="begbstcan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                    <tr>
-                        <td>Water</td>
-                        <td><input type="number" class="textbox" id="begwatercase" name="begwatercase" placeholder="" style="width:200px" /></td>
-                        <td><input type="number" class="textbox" id="begwatercan" name="begwatercan" placeholder="" style="width:200px" /></td>
-                    </tr>
-                </table>
+                <div>
+                    <center><img src="https://taftandhardingevents.com/wp-content/uploads/2022/06/taft-and-harding-final.png" height=150 width=180 alt="logo"></center>
+                    <table id="beer">
+                        <tr>
+                            <th>Beer</th>
+                            <th>Cases</th>
+                            <th>Cans</th>
+                        </tr>
+                        <tr>
+                            <td>Coors</td>
+                            <td><input type="number" class="textbox" id="begcoorcase" name="begcoorcase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begcoorcan" name="begcoorcan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                        <tr>
+                            <td>Miller</td>
+                            <td><input type="number" class="textbox" id="begmillercase" name="begmillercase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begmillercan" name="begmillercan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                        <tr>
+                            <td>Vizzy</td>
+                            <td><input type="number" class="textbox" id="begvizzycase" name="begvizzycase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begvizzycan" name="begvizzycan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                        <tr>
+                            <td>Topo Chico</td>
+                            <td><input type="number" class="textbox" id="begtopocase" name="begtopocase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begtopocan" name="begtopocan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                        <tr>
+                            <td>R&D Carolina</td>
+                            <td><input type="number" class="textbox" id="begrdccase" name="begrdccase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begrdccan" name="begrdccan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                        <tr>
+                            <td>R&D MTN</td>
+                            <td><input type="number" class="textbox" id="begrdmcase" name="begrdmcase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begrdmcan" name="begrdmcan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                        <tr>
+                            <td>Booneshine Tropic</td>
+                            <td><input type="number" class="textbox" id="begbstcase" name="begbstcase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begbstcan" name="begbstcan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                        <tr>
+                            <td>Water</td>
+                            <td><input type="number" class="textbox" id="begwatercase" name="begwatercase" placeholder="" style="width:200px" /></td>
+                            <td><input type="number" class="textbox" id="begwatercan" name="begwatercan" placeholder="" style="width:200px" /></td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+                <div>&nbsp;&nbsp;
+                    <input type="submit" value="Submit" name="submit_but" id="submit_but" style="width:100px; height: 30px;" />
             </form>
         </div>
         <br>
-        <div>
-            <form method="post" action="">&nbsp;&nbsp;
-                <input type="submit" value="Submit" name="submit_but" id="submit_but" style="width:100px; height: 30px;" />
-            </form>
-        </div>
-        <br>
-    </center>
-    <?php
-    // getting values from HTML form
-    if (isset($_POST['submit_but'])) {
-        $barid = $_REQUEST['barid'];
-        $barname = $_REQUEST['barname'];
-        $manname = $_REQUEST['man_name'];
-        $date = $_REQUEST['txt_date'];
+        </form>
+</div>
+</center>
+<?php
 
-        $begcoorscase = $_REQUEST['begcoorcase'];
-        $begcoorscan = $_REQUEST['begcoorcan'];
-        $begmillercase = $_REQUEST['begmillercase'];
-        $begmillercan = $_REQUEST['begmillercan'];
-        $begvizzycase = $_REQUEST['begvizzycase'];
-        $begvizzycan = $_REQUEST['begvizzycan'];
-        $begtopocase = $_REQUEST['begtopocase'];
-        $begtopocan = $_REQUEST['begtopocan'];
-        $begrdccase = $_REQUEST['begrdccase'];
-        $begrdccan = $_REQUEST['begrdccan'];
-        $begrdmcase = $_REQUEST['begrdmcase'];
-        $begrdmcan = $_REQUEST['begrdmcan'];
-        $begbstcase = $_REQUEST['begbstcase'];
-        $begbstcan = $_REQUEST['begbstcan'];
-        $begwatercase = $_REQUEST['begwatercase'];
-        $begwaterbottle = $_REQUEST['begwatercan'];
-    }
+// database login details
+$host = "localhost";
+$username = "root";
+$password = "";
+$dnbame = "database";
 
-    // database login details
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dnbame = "database";
+// creating connection
+$con = mysqli_connect($host, $username, $password, $dbname);
 
-    // creating connection
-    $con = mysqli_connect($host, $username, $password, $dbname);
+// ensure connection is made
+if (!$con) {
+    die("Connection failed!" . mysqli_connect_error());
+}
 
-    // ensure connection is made
-    if (!$con) {
-        die("Connection failed!" . mysqli_connect_error());
-    }
+// getting values from HTML form
+if (isset($_POST['submit_but'])) {
+    $barid = $_POST['barid'];
+    $barname = $_POST['barname'];
+    $manname = $_POST['man_name'];
+    $date = $_POST['txt_date'];
 
-    // using SQL to create a data entry query
-    $sql = "INSERT INTO `inv_entries` (`barid`, `barname`, `manname`, `date`, `begcoorscase`, `begcoorscan`, `begmillercase`, `begmillercan`, `begvizzycase`, `begvizzycan`, `begtopocase`, `begtopocan`, `begrdccase`, `begrdccan`, `begrdmcase`, `begrdmcan`, `begwatercase`, `begwaterbottle`) VALUES ('$barid', '$barname', '$manname', '$date', '$begcoorscase', '$begcoorscan', '$begmillercase', '$begmillercan', '$begvizzycase', '$begvizzycan', '$begtopocase', '$begtopocan', '$begrdccase', '$begrdccan', '$begrdmcase', '$begrdmcan', '$begbstcase', '$begbstcan', '$begwatercase', '$begwaterbottle')";
+    $begcoorscase = $_POST['begcoorcase'];
+    $begcoorscan = $_POST['begcoorcan'];
+    $begmillercase = $_POST['begmillercase'];
+    $begmillercan = $_POST['begmillercan'];
+    $begvizzycase = $_POST['begvizzycase'];
+    $begvizzycan = $_POST['begvizzycan'];
+    $begtopocase = $_POST['begtopocase'];
+    $begtopocan = $_POST['begtopocan'];
+    $begrdccase = $_POST['begrdccase'];
+    $begrdccan = $_POST['begrdccan'];
+    $begrdmcase = $_POST['begrdmcase'];
+    $begrdmcan = $_POST['begrdmcan'];
+    $begbstcase = $_POST['begbstcase'];
+    $begbstcan = $_POST['begbstcan'];
+    $begwatercase = $_POST['begwatercase'];
+    $begwaterbottle = $_POST['begwatercan'];
+}
 
-    // send query to the database to add values and confirm if successful
-    $rs = mysqli_query($con, $sql);
-    if ($rs) {
-        echo "Entries added!";
-    }
+// using SQL to create a data entry query
+$sql = "INSERT INTO beginv (coorCase, coorCan) VALUES ($begcoorscase, $begcoorscan)";
 
-    // close connection
-    mysqli_close($con);
+// send query to the database to add values and confirm if successful
+if (mysqli_query($con, $sql)) {
+    echo "SQL Query Successful";
+}
 
-    ?>
-    <br><br><br><br>
+// close connection
+mysqli_close($con);
+
+?>
+<br><br><br><br>
 </div>
 
 </html>
